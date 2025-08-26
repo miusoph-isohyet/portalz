1253220db28536afee4f15e54e8600cefd3019c9
crocusezes <nicholasodeanjohnson42@gmail.com>
2025-08-26 18:43:54 -0500

    Create 2.js

---
+++ src/2.js
@@ -0,0 +1,26 @@
+(() => {
+  const hashTrigger = '#';
+  const targetHTML = 'img/3.html';
+  const containerId = 'image-container';
+
+  function injectHTML(file) {
+    fetch(`img/${file}`)
+      .then(response => response.text())
+      .then(html => {
+        const container = document.getElementById(containerId);
+        if (container) container.innerHTML = html;
+      })
+      .catch(err => {
+        console.error(`Error loading ${file}:`, err);
+      });
+  }
+
+  function evaluateHash() {
+    if (window.location.hash === hashTrigger) {
+      injectHTML(targetHTML);
+    }
+  }
+
+  window.addEventListener('hashchange', evaluateHash);
+  evaluateHash();
+})();
