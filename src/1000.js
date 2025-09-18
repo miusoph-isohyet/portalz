7a9231d4c37aa95a4fe95d6bb3808ff768038b42
crocuses <nicholasodeanjohnson42@gmail.com>
2025-09-18 14:13:53 -0500

    Add files via upload

---
+++ src/1000.js
@@ -0,0 +1,26 @@
+(() => {
+  const hashTrigger = '#2672218645582630320484230778847759';
+  const targetHTML = '1000.html';
+  const containerId = 'image-container';
+
+  function injectHTML(file) {
+    fetch(`img/${file}`)
+      .then(response => response.text())
+      .then(html => {
+        const container = document.getElementById(containerId);
+        if (container) container.innerHTML = html;
+      })
+      .catch(err => {
+        console.error(`Error loading ${file}:`, err);
+      });
+  }
+
+  function evaluateHash() {
+    if (window.location.hash === hashTrigger) {
+      injectHTML(targetHTML);
+    }
+  }
+
+  window.addEventListener('hashchange', evaluateHash);
+  evaluateHash();
+})();
